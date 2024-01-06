bde21e393e3db8df35afad0142679bda70dc5c70
HectorGarciaCarlota <soyunpenejaja@gmail.com>
2024-01-06 18:42:01 +0100

    Arreglat api

--- app/src/main/java/com/example/soundrater1/MainActivity.kt
+++ app/src/main/java/com/example/soundrater1/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.soundrater1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Website.URL
+import android.util.Log
+import android.widget.Button
+import com.google.gson.internal.bind.TypeAdapters.URL
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
@@ -9,10 +14,17 @@ import com.spotify.protocol.types.Track
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
 import com.spotify.sdk.android.auth.AuthorizationClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+   /* override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -21,6 +33,77 @@ class MainActivity : AppCompatActivity() {
         val request = builder.build()
 
         AuthorizationClient.openLoginInBrowser(this, request)
+    }
+
+    */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spotify_login_btn = findViewById<Button>(R.id.spotify_login_btn)
+        spotify_login_btn.setOnClickListener {
+            val request = getAuthenticationRequest(AuthorizationResponse.Type.TOKEN)
+            AuthorizationClient.openLoginActivity(
+                this,
+                SpotifyApi.AUTH_TOKEN_REQUEST_CODE,
+                request
+            )
+        }
+    }
+    private fun getAuthenticationRequest(type: AuthorizationResponse.Type): AuthorizationRequest {
+        return AuthorizationRequest.Builder(SpotifyApi.CLIENT_ID, type, SpotifyApi.REDIRECT_URI)
+            .setShowDialog(false)
+            .setScopes(arrayOf("user-read-email"))
+            .build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (SpotifyApi.AUTH_TOKEN_REQUEST_CODE == requestCode) {
+            val response = AuthorizationClient.getResponse(resultCode, data)
+            val accessToken: String? = response.accessToken
+            fetchSpotifyUserProfile(accessToken)
+        }
+    }
+    private fun fetchSpotifyUserProfile(token: String?) {
+        Log.d("Status: ", "Please Wait...")
+        if (token == null) {
+            Log.i("Status: ", "Something went wrong - No Access Token found")
+            return
+        }
+        val getUserProfileURL = "https://api.spotify.com/v1/me"
+        GlobalScope.launch(Dispatchers.Default) {
+            val url = URL(getUserProfileURL)
+            val httpsURLConnection = withContext(Dispatchers.IO) {url.openConnection() as HttpsURLConnection }
+            httpsURLConnection.requestMethod = "GET"
+            httpsURLConnection.setRequestProperty("Authorization", "Bearer $token")
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = false
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                .use { it.readText() }  // defaults to UTF-8
+            withContext(Dispatchers.Main) {
+                val jsonObject = JSONObject(response)
+                // Spotify Id
+                val spotifyId = jsonObject.getString("id")
+                Log.d("Spotify Id :", spotifyId)
+                // Spotify Display Name
+                val spotifyDisplayName = jsonObject.getString("display_name")
+                Log.d("Spotify Display Name :", spotifyDisplayName)
+                // Spotify Email
+                val spotifyEmail = jsonObject.getString("email")
+                Log.d("Spotify Email :", spotifyEmail)
+                val spotifyAvatarArray = jsonObject.getJSONArray("images")
+                //Check if user has Avatar
+                var spotifyAvatarURL = ""
+                if (spotifyAvatarArray.length() > 0) {
+                    spotifyAvatarURL = spotifyAvatarArray.getJSONObject(0).getString("url")
+                    Log.d("Spotify Avatar : ", spotifyAvatarURL)
+                }
+                Log.d("Spotify AccessToken :", token)
+
+            }
+        }
     }
 
     // Rest of your class code (e.g., onStart, onStop)
